543384d75cdddfc22b0a945038a6198c1fbbcf8d
Lukáš Stuchlík <stuchlik1@seznam.cz>
2018-12-10 01:08:57 +0100

    Refactor, added CSV export

--- src/Result.js
+++ src/Result.js
@@ -1,8 +1,9 @@
 import React from 'react';
-import { Col } from 'reactstrap'
+import {Col, Row} from 'reactstrap'
 import PropTypes from 'prop-types';
 import ResultTable from "./ResultTable";
 import ResultGraph from "./ResultGraph";
+import {CSVLink} from "react-csv";
 
 export default class Result extends React.Component {
   static propTypes = {
@@ -16,6 +17,29 @@ export default class Result extends React.Component {
         )
       )
     ).isRequired,
+  };
+
+  constructor(options) {
+    super(options);
+    this.cropRealPopulation = this.cropRealPopulation.bind(this);
+  }
+
+  realPopulations = {
+    chrome: {
+      name: 'Chrome',
+      population: [70.6, 71, 71.9, 72.5, 73.4, 73.6, 73.8, 74.2, 74.5, 75, 75.8, 75.7, 75.7, 76.1, 77.1, 77.7, 77.8, 78.3, 78.7, 78.9, 78.6, 78.6, 78.9, 79.2, 79.3, 79.9, 80.2, 80.7, 81, 81.3, 81.9, 81.8, 81.6, 81.6],
+      color: '#66a3ff'
+    },
+    firefox: {
+      name: 'Firefox',
+      population: [21, 20.8, 20, 19.6, 18.9, 18.9, 19.1, 18.8, 18.3, 17.8, 17.3, 17.5, 17.4, 17, 16.1, 15.6, 15.6, 15.3, 15.3, 15.1, 15, 14.6, 14.6, 14.6, 14.5, 13.8, 13.8, 13.3, 13.2, 13, 12.8, 12.6, 12.5, 12.3],
+      color: '#ffa366'
+    },
+    edge: {
+      name: 'IE/Edge',
+      population: [8.4, 8.2, 8.1, 7.9, 7.7, 7.5, 7.1, 7, 7.2, 7.2, 6.9, 6.8, 6.9, 6.9, 6.8, 6.7, 6.6, 6.4, 6, 6, 6.4, 6.8, 6.5, 6.2, 6.2, 6.3, 6, 6, 5.8, 5.7, 5.3, 5.6, 5.9, 6.1],
+      color: '#66ff66',
+    },
   };
 
   get flattenPopulations() {
@@ -51,15 +75,50 @@ export default class Result extends React.Component {
     return result;
   }
 
+  cropRealPopulation(count) {
+    return Object.entries(this.realPopulations).reduce((obj, [key, browser]) => {
+      obj[key] = {
+        name: browser.name,
+        color: browser.color,
+        population: browser.population.slice(0, Math.min(count, browser.population.length)),
+      };
+      return obj;
+    }, {});
+  }
+
+  get csvData() {
+    const count = this.props.populations.length;
+    const real = this.cropRealPopulation(count);
+    const simulated = this.populationsByBrowser;
+    const ret = {
+      headers: ['Prohlížeč', ...[...Array(count)].map((_, i) => `${i + 1}. měsíc`)],
+      data: [
+        ...Object.values(real).map(browser => [`${browser.name} - skutečný`, ...browser.population]),
+        ...Object.values(simulated).map(browser => [`${browser.name} - simulovaný`, ...browser.population]),
+      ]
+    };
+    console.log('csvData(), ret', ret);
+    return ret;
+  }
+
   render() {
     const populationCount = this.props.populations.length;
     const byBrowser = this.populationsByBrowser;
+    const croppedReal = this.cropRealPopulation(populationCount);
+    const csv = this.csvData;
     return (
       <>
-        <ResultTable count={populationCount} populations={byBrowser} />
-        <Col sm={{ size: 8, offset: 2 }}>
-          <ResultGraph count={populationCount} browsers={byBrowser} />
-        </Col>
+        <Row>
+          <ResultTable count={populationCount} populations={byBrowser} />
+        </Row>
+        <Row>
+          <Col sm={{ size: 8, offset: 2 }}>
+            <ResultGraph realData={croppedReal} browsers={byBrowser} />
+          </Col>
+        </Row>
+        <Row>
+          <CSVLink filename="data.csv" headers={csv.headers} data={csv.data}>Export do CSV</CSVLink>
+        </Row>
       </>
     );
   }
--- src/ResultGraph.js
+++ src/ResultGraph.js
@@ -4,39 +4,9 @@ import { LineSeries, YAxis, HorizontalGridLines, DiscreteColorLegend, FlexibleWi
 
 export default class ResultGraph extends React.Component {
   static propTypes = {
-    count: PropTypes.number.isRequired,
     browsers: PropTypes.object.isRequired,
+    realData: PropTypes.object.isRequired,
   };
-
-  realPopulations = {
-    chrome: {
-      name: 'Chrome',
-      population: [70.6, 71, 71.9, 72.5, 73.4, 73.6, 73.8, 74.2, 74.5, 75, 75.8, 75.7, 75.7, 76.1, 77.1, 77.7, 77.8, 78.3, 78.7, 78.9, 78.6, 78.6, 78.9, 79.2, 79.3, 79.9, 80.2, 80.7, 81, 81.3, 81.9, 81.8, 81.6, 81.6],
-      color: '#66a3ff'
-    },
-    firefox: {
-      name: 'Firefox',
-      population: [21, 20.8, 20, 19.6, 18.9, 18.9, 19.1, 18.8, 18.3, 17.8, 17.3, 17.5, 17.4, 17, 16.1, 15.6, 15.6, 15.3, 15.3, 15.1, 15, 14.6, 14.6, 14.6, 14.5, 13.8, 13.8, 13.3, 13.2, 13, 12.8, 12.6, 12.5, 12.3],
-      color: '#ffa366'
-    },
-    edge: {
-      name: 'IE/Edge',
-      population: [8.4, 8.2, 8.1, 7.9, 7.7, 7.5, 7.1, 7, 7.2, 7.2, 6.9, 6.8, 6.9, 6.9, 6.8, 6.7, 6.6, 6.4, 6, 6, 6.4, 6.8, 6.5, 6.2, 6.2, 6.3, 6, 6, 5.8, 5.7, 5.3, 5.6, 5.9, 6.1],
-      color: '#66ff66',
-    },
-  };
-
-  cropRealPopulation(count) {
-    const copy = Object.entries(this.realPopulations).reduce((obj, [key, browser]) => {
-      obj[key] = {
-        name: browser.name,
-        color: browser.color,
-        population: browser.population.slice(0, Math.min(count, browser.population.length)),
-      };
-      return obj;
-    }, {});
-    return copy;
-  }
 
   mapToXY(population) {
     return population.map((value, i) => ({ x: i, y: value }));
@@ -51,8 +21,7 @@ export default class ResultGraph extends React.Component {
   }
 
   render() {
-    const { count, browsers } = this.props;
-    const real = this.cropRealPopulation(count);
+    const { realData, browsers } = this.props;
     return (
       <FlexibleWidthXYPlot
         height={400}
@@ -61,11 +30,11 @@ export default class ResultGraph extends React.Component {
         <HorizontalGridLines />
         <DiscreteColorLegend
           items={[
-            ...this.getLegend(real),
+            ...this.getLegend(realData),
             ...this.getLegend(browsers, true)
           ]}
           orientation="horizontal" />
-        {Object.values(real).map(browser => (
+        {Object.values(realData).map(browser => (
           <LineSeries
           data={this.mapToXY(browser.population)}
           color={browser.color} />
